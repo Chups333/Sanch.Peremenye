14ebfd93f527b5a18c6b51d45a299b630382100d
Petrov Sasha <sasha1327@mail.ru>
2020-08-20 18:27:53 +0800

    Добавьте файлы проекта.

---
+++ Sanch.Peremenye/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanch.Peremenye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            i = 6;
+            //i = "asa" - нельзя
+            i = -31;
+
+            double j = 5;
+            j = 5.0;
+            j = 4.3;
+
+            char c = 'a';
+            c = '1';
+
+            bool d = true;
+            d = false;
+
+            string s = "hello";
+            var ss = "world dasdasd asdsd asd ";
+
+            decimal e = 0.1M;
+
+            float t = 0.1F;
+
+            int y = 5;
+            dynamic o = 5;
+
+           // y.sadasdsd(); - ошибка
+           // o.asdasdasd(); - нет ошибки (тип позволяет)
+        }
+    }
+}
